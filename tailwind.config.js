5a07795c4a93894ae849443a1f6a2ab134b5626a
Dropheart <ahmed@techfromtheheart.com>
2023-07-19 20:25:52 +0300

    pushing files to git

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          'hanuman': ['Hanuman', 'serif']
+        },
+        colors: {
+          'grey': '#d9d9d9',
+          'dark-bg': '#231f1f'
+        }
+      },
+      container: {
+        center: true,
+      }
+    },
+    plugins: [],
+  }
